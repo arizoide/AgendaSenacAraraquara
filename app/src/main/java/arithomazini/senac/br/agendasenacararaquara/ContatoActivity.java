a8aeead6c16a65b24eef18a4c63b13a2d606c292
ari.thomazini <ari.thomazini@gmail.com>
2019-02-12 21:10:42 -0200

    Commit inicial.

--- app/src/main/java/arithomazini/senac/br/agendasenacararaquara/ContatoActivity.java
+++ app/src/main/java/arithomazini/senac/br/agendasenacararaquara/ContatoActivity.java
@@ -2,6 +2,9 @@ package arithomazini.senac.br.agendasenacararaquara;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class ContatoActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class ContatoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contato);
+
+        // ******************************
+        // Acao de click no salvar
+        // ******************************
+
+        //Recuperto o botao de salvar
+        Button salvarContatoButton = findViewById(R.id.salvarContatoButton);
+
+        salvarContatoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ContatoActivity.this,
+                        "Contato Salvo!",
+                        Toast.LENGTH_SHORT).show();
+
+                finish();
+            }
+        });
+
     }
 }
